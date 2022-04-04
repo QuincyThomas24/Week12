d135a2a7a85cefa3db1e02583244b644df00bc13
Thomas <24qthomas@FVFY37ADJ1WK.tld>
2022-04-04 13:04:05 -0500

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+let userNum = document.getElementById("getNumber")
+userNum.addEventListener("keydown", function (e) {
+    if (e.keyCode === 13) {
+        run(e)
+    }
+});
+
+userNum.focus()
+
 
 function run() {
 document.getElementById("paragraph"). innerHTML = "Hello World!!!";
@@ -5,6 +14,8 @@ document.getElementById("paragraph").style.backgroundColor = "blue";
 document.getElementById("paragraph").style.color = "white";
 document.getElementById("paragraph").style.padding = "20px";
 randomNum()
+userNumber()
+compareNumbers()
 }
 
 function randomNum() {
@@ -15,17 +26,43 @@ function randomNum() {
     x.style.color = "white";
     x.style.padding = "20px";
     x.style.textAlign = "center";
-
+    return ran
 }
 
 function userNumber() {
 let user = document.getElementById("getNumber").value;
-alert(user)
+let y = document.getElementById("userNumber")
+y.innerHTML = user;
+y.style.color = "white";
+y.style.backgroundColor = "orange";
+y.style.padding = "20px"
+y.style.textAlign = "center";
+return user 
 }
 
+function compareNumbers() {
+let a = randomNum()
+let b = userNumber()
+let z = document.getElementById("compare")
+if ( a!= b) {
+   z.innerHTML = "Numbers are not the same. Computer got " + a + ", and user got "
++ b;
+z.style.color = "white";
+z.style.backgroundColor = "purple";
+z.style.padding = "20px"
+z.style.textAlign = "center";
+} else if (a == b)
+z.innerHTML = "Numbers are the same. Computer got " + a + ", and user got "
++ b;
+z.style.color = "white";
+z.style.backgroundColor = "green";
+z.style.padding = "20px"
+z.style.textAlign = "center";
+}
 
-
-
+function resetInput() {
+    document.getElementById("")
+}
 
 
 
